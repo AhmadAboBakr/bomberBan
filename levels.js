ad0f229cce170553fec06cb7fde7928dd3dc5a94
AhmadAboBakr <ahmadabobakr@gmail.com>
2014-12-22 07:10:47 +0000

    Initial commit
    player can push boxes
    boxes collide with each other
    player and boces collide with walls

---
+++ levels.js
@@ -0,0 +1,11 @@
+var levels = [
+    {
+        width: 10 ,
+        height: 10,
+        boxes: [{ x: 2, y: 2 }, { x: 7, y: 7 }],
+        exit:{x:10,y:10},
+        walls: [{ x: 5, y: 2 }, { x: 5, y: 3}],
+        player: {x:5,y:5}
+    }
+
+];
